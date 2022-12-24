ef985a6b1256b1082cbab83b7223b0abf25d3253
chris1165 <christian.paez.1@cmsysconsulting.io>
2022-12-24 14:36:58 -0400

    feature: desarrollo primera parte

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,20 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use App\Traits\ApiResponse;
+use Illuminate\Http\Response;
+
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponse;
     /**
      * A list of the exception types that are not reported.
      *
@@ -37,5 +46,63 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Throwable
+     */
+    public function render($request, Throwable $exception)
+    {
+       
+        if ($exception instanceof ValidationException) {
+            return $this->convertValidationExceptionToResponse($exception, $request);
+        }
+
+        /* if (($exception instanceof UnauthorizedException) || ($exception instanceof AuthorizationException) ) {
+            
+            return $this->errorResponse("You do not have permissions to access this route", Response::HTTP_FORBIDDEN);
+        } */
+
+        /* if ($exception instanceof AuthenticationException) {
+            return $this->errorResponse("You must login", Response::HTTP_UNAUTHORIZED);
+        } */
+       
+        if ($exception instanceof ConnectionException) {
+            return $this->showMessage("Unable to establish connection to external server", Response::HTTP_REQUEST_TIMEOUT);
+        }
+       
+        if ($exception instanceof ModelNotFoundException) {
+            return $this->showMessage("Does not exist any record", Response::HTTP_NOT_FOUND);
+        }
+
+        if ($exception instanceof NotFoundHttpException) {
+            return $this->showMessage('The given path does not exist', Response::HTTP_NOT_FOUND);
+        }
+
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return $this->showMessage('The method does not exist', Response::HTTP_NOT_FOUND);
+        }
+
+        if ($exception instanceof \Exception) {
+            return $this->showMessage($exception->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        if ($exception instanceof \TypeError) {
+            return $this->showMessage($exception->getMessage(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        return parent::render($request, $exception);
+    }
+
+    protected function convertValidationExceptionToResponse(ValidationException $e, $request)
+    {
+        $errors = $e->validator->errors()->getMessages();
+
+        return $this->showMessage($errors, 422);
     }
 }
